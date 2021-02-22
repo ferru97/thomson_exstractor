a014f574199d6b206d72a5f858975a8537500082
Vito Ferrulli <vitof970@gmail.com>
2021-02-22 11:44:55 +0100

    Fix

--- utils.py
+++ utils.py
@@ -18,6 +18,14 @@ MonthDict={ "january":1,
             "december":12
 }
 
+def find_all(a_str, sub):
+    start = 0
+    while True:
+        start = a_str.find(sub, start)
+        if start == -1: return
+        yield start
+        start += len(sub)
+
 def isPersonName(name):
     soup = BeautifulSoup(name, 'html.parser')
     name = soup.text.strip()
